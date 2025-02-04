35e6b7b1d545c361a126fa4231acafd0bd729513
est22 <anlia.seoul@gmail.com>
2025-02-04 19:39:53 +0900

    ✨ feat: add middleware and router

--- server/app.js
+++ server/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const cors = require('cors');
+const { initializeDatabase } = require('./models/model');
+const techInnovationRoutes = require('./routes/techInnovationRoutes');
+
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-}); // get 요청만 받겠다
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// tech innovation routes
+app.use('/', techInnovationRoutes);
 
 
+// start server
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
-  });
+});
